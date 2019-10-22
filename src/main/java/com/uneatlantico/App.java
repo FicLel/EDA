bd76e09e9a42fcf0ac25d187741e71400bcedac0
FicLel <victorleal190@gmail.com>
2019-10-22 19:43:59 +0200

    initial commit, farewell

---
+++ src/main/java/com/uneatlantico/App.java
@@ -0,0 +1,13 @@
+package com.uneatlantico;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
