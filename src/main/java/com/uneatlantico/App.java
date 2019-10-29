e4e3bbfad52200b39d046b1cc21d684eda4532c9
FicLel <victorleal190@gmail.com>
2019-10-29 20:02:12 +0100

    asd

--- src/main/java/com/uneatlantico/App.java
+++ src/main/java/com/uneatlantico/App.java
@@ -1,13 +1,16 @@
 package com.uneatlantico;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        Reader r = new Reader();
+        r.readFile();
     }
 }
--- src/main/java/com/uneatlantico/Reader.java
+++ src/main/java/com/uneatlantico/Reader.java
@@ -1,5 +1,36 @@
 package com.uneatlantico;
 
-public class Reader {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 
+public class Reader {
+	public static final String SEPARATOR=",";
+	public static final String QUOTE="\"";
+	public void readFile() throws IOException {
+		BufferedReader lector = null; 
+		try { 
+			lector =new BufferedReader(new FileReader("Resources/airports.csv")); 
+			String line = lector.readLine(); 
+			while (null != line) { 
+				String [] fields = line.split(SEPARATOR); 
+				System.out.println(Arrays.toString(fields));  
+				line = lector.readLine(); 
+			} 
+		} catch (Exception e) { 
+			System.out.println(e); 
+		} finally { 
+			if (null!=lector) { 
+				try {
+					lector.close();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+			} 
+		}	
+	}
+	
 }
+
