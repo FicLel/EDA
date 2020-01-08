3a5ddac38e02986521817e162641ccd76f165309
FicLel <victorleal190@gmail.com>
2020-01-08 21:43:06 +0100

    nel

--- src/main/java/com/uneatlantico/App.java
+++ src/main/java/com/uneatlantico/App.java
@@ -22,19 +22,19 @@ public class App
     {
       
       
-        Reader r = new Reader();
-        ArrayList<Airport> airportes = r.readFileAirports();
-        ArrayList<Route> routes = r.readFileRoutes(airportes);
-        Graph graph = new MultiGraph("World");
-        graph.setAttribute("ui.quality");
-        addNodesToGraph(airportes,graph);
-        addEdgesToGraph(routes,graph);
-        graph.setAttribute("ui.stylesheet", style());
-        graph.addAttribute("ui.screenshot", "url('https://www.mapsland.com/maps/world/large-satellite-map-of-the-world.jpg')");
-        Viewer viewer = graph.display(false);
-        viewer.disableAutoLayout();
-//    	MainUI main = new MainUI();
-//    	main.setVisible(true);
+//        Reader r = new Reader();
+//        ArrayList<Airport> airportes = r.readFileAirports();
+//        ArrayList<Route> routes = r.readFileRoutes(airportes);
+//        Graph graph = new MultiGraph("World");
+//        graph.setAttribute("ui.quality");
+//        addNodesToGraph(airportes,graph);
+//        addEdgesToGraph(routes,graph);
+//        graph.setAttribute("ui.stylesheet", style());
+//        graph.addAttribute("ui.screenshot", "url('https://www.mapsland.com/maps/world/large-satellite-map-of-the-world.jpg')");
+//        Viewer viewer = graph.display(false);
+//        viewer.disableAutoLayout();
+    	MainUI main = new MainUI();
+    	main.setVisible(true);
 
         
     }
